62aa80b8b30d87d54f1073380cac35572b8179a6
Ali Muzaffar <atgheb@gmail.com>
2012-08-06 10:46:22 +1000

    added LocationUtils. disabled DEBUG, converted to library.

--- gen/com/alimuzaffar/sunalarm/R.java
+++ gen/com/alimuzaffar/sunalarm/R.java
@@ -11,75 +11,75 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        public static final int padding_large=0x7f040002;
-        public static final int padding_medium=0x7f040001;
-        public static final int padding_small=0x7f040000;
+        public static int padding_large=0x7f040002;
+        public static int padding_medium=0x7f040001;
+        public static int padding_small=0x7f040000;
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static int ic_action_search=0x7f020000;
+        public static int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int alarmDialog=0x7f080000;
-        public static final int dawn=0x7f080005;
-        public static final int dawnAlarm=0x7f08000b;
-        public static final int dawnAlarmSet=0x7f08000c;
-        public static final int dawnTime=0x7f080006;
-        public static final int delayDawnAlarm=0x7f080012;
-        public static final int delayDuskAlarm=0x7f080015;
-        public static final int delay_dawn_alarm=0x7f080011;
-        public static final int delay_dusk_alarm=0x7f080014;
-        public static final int dusk=0x7f080008;
-        public static final int duskAlarm=0x7f08000e;
-        public static final int duskAlarmSet=0x7f08000f;
-        public static final int duskTime=0x7f080009;
-        public static final int focussucker=0x7f080003;
-        public static final int menu_settings=0x7f080019;
-        public static final int scrollView1=0x7f080002;
-        public static final int tableRow1=0x7f080004;
-        public static final int tableRow2=0x7f080007;
-        public static final int tableRow3=0x7f08000a;
-        public static final int tableRow4=0x7f08000d;
-        public static final int tableRow5=0x7f080010;
-        public static final int tableRow6=0x7f080013;
-        public static final int tableRow7=0x7f080016;
-        public static final int testDawn=0x7f080017;
-        public static final int testDusk=0x7f080018;
-        public static final int turnAOffAlarm=0x7f080001;
+        public static int alarmDialog=0x7f080000;
+        public static int dawn=0x7f080005;
+        public static int dawnAlarm=0x7f08000b;
+        public static int dawnAlarmSet=0x7f08000c;
+        public static int dawnTime=0x7f080006;
+        public static int delayDawnAlarm=0x7f080012;
+        public static int delayDuskAlarm=0x7f080015;
+        public static int delay_dawn_alarm=0x7f080011;
+        public static int delay_dusk_alarm=0x7f080014;
+        public static int dusk=0x7f080008;
+        public static int duskAlarm=0x7f08000e;
+        public static int duskAlarmSet=0x7f08000f;
+        public static int duskTime=0x7f080009;
+        public static int focussucker=0x7f080003;
+        public static int menu_settings=0x7f080019;
+        public static int scrollView1=0x7f080002;
+        public static int tableRow1=0x7f080004;
+        public static int tableRow2=0x7f080007;
+        public static int tableRow3=0x7f08000a;
+        public static int tableRow4=0x7f08000d;
+        public static int tableRow5=0x7f080010;
+        public static int tableRow6=0x7f080013;
+        public static int tableRow7=0x7f080016;
+        public static int testDawn=0x7f080017;
+        public static int testDusk=0x7f080018;
+        public static int turnAOffAlarm=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_alarm=0x7f030000;
-        public static final int activity_show_status=0x7f030001;
+        public static int activity_alarm=0x7f030000;
+        public static int activity_show_status=0x7f030001;
     }
     public static final class menu {
-        public static final int show_status=0x7f070000;
+        public static int show_status=0x7f070000;
     }
     public static final class string {
-        public static final int alarm_auto_off=0x7f050010;
-        public static final int alarm_auto_off_description=0x7f050011;
-        public static final int alert_description=0x7f050013;
-        public static final int app_name=0x7f050000;
-        public static final int dawn=0x7f050001;
-        public static final int dawnAlarm=0x7f050003;
-        public static final int delay_dawn_alarm=0x7f050004;
-        public static final int delay_dusk_alarm=0x7f050008;
-        public static final int dusk=0x7f050005;
-        public static final int duskAlarm=0x7f050007;
-        public static final int int_0=0x7f05000c;
-        public static final int menu_settings=0x7f05000a;
-        public static final int ring_alarm=0x7f05000f;
-        public static final int s_dawn=0x7f050002;
-        public static final int s_dusk=0x7f050006;
-        public static final int test_dawn=0x7f050014;
-        public static final int test_dusk=0x7f050015;
-        public static final int time=0x7f050009;
-        public static final int title_activity_show_status=0x7f05000b;
-        public static final int today=0x7f05000d;
-        public static final int tomorrow=0x7f05000e;
-        public static final int turn_off_alarm=0x7f050012;
+        public static int alarm_auto_off=0x7f050010;
+        public static int alarm_auto_off_description=0x7f050011;
+        public static int alert_description=0x7f050013;
+        public static int app_name=0x7f050000;
+        public static int dawn=0x7f050001;
+        public static int dawnAlarm=0x7f050003;
+        public static int delay_dawn_alarm=0x7f050004;
+        public static int delay_dusk_alarm=0x7f050008;
+        public static int dusk=0x7f050005;
+        public static int duskAlarm=0x7f050007;
+        public static int int_0=0x7f05000c;
+        public static int menu_settings=0x7f05000a;
+        public static int ring_alarm=0x7f05000f;
+        public static int s_dawn=0x7f050002;
+        public static int s_dusk=0x7f050006;
+        public static int test_dawn=0x7f050014;
+        public static int test_dusk=0x7f050015;
+        public static int time=0x7f050009;
+        public static int title_activity_show_status=0x7f05000b;
+        public static int today=0x7f05000d;
+        public static int tomorrow=0x7f05000e;
+        public static int turn_off_alarm=0x7f050012;
     }
     public static final class style {
-        public static final int AppDialog=0x7f060001;
-        public static final int AppTheme=0x7f060000;
+        public static int AppDialog=0x7f060001;
+        public static int AppTheme=0x7f060000;
     }
 }
