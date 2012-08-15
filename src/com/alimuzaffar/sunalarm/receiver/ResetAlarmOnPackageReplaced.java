7c64064af5149188dfe7971602f5f834b380c4d9
Ali Muzaffar <atgheb@gmail.com>
2012-08-15 11:58:35 +1000

    Added code to fix alarm breaking on application update problem.

---
+++ src/com/alimuzaffar/sunalarm/receiver/ResetAlarmOnPackageReplaced.java
@@ -0,0 +1,36 @@
+package com.alimuzaffar.sunalarm.receiver;
+
+import com.alimuzaffar.sunalarm.util.AppSettings;
+import com.alimuzaffar.sunalarm.util.AppSettings.Key;
+import com.alimuzaffar.sunalarm.util.Utils;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class ResetAlarmOnPackageReplaced extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		try {
+			if (intent.getDataString().contains(context.getPackageName())) {
+	            AppSettings settings = AppSettings.getInstance(context);
+	            if(settings.getBoolean(Key.DAWN_ALARM)) {
+	            	Utils.stopAlarm(context, Key.DAWN_ALARM.toString());
+	            	Utils.setAlarm(context, Key.DAWN_ALARM.toString());
+	            } else if (settings.getBoolean(Key.DUSK_ALARM)) {
+	            	Utils.stopAlarm(context, Key.DUSK_ALARM.toString());
+	            	Utils.setAlarm(context, Key.DUSK_ALARM.toString());
+	            }
+	        }	
+			
+		} catch (Exception e) {
+			Log.e(this.getClass().getName(), e.getMessage(), e);
+
+		}
+
+	}
+	
+
+}
